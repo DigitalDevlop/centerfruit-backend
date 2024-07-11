94097be2d4b36aafefb79c0451236f57ab9ce81a
Navod Amarakoon <nexza@Users-MacBook-3.local>
2024-07-11 14:26:28 +0530

    Add Daraz Intigration Template

--- src/config/template.js
+++ src/config/template.js
@@ -7,5 +7,5 @@
 module.exports = {
     newPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
     existingPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
-    darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. {voucher}`,
+    darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. Voucher Code {voucher}`,
 };
