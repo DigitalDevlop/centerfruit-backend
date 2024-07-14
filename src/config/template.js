f7683576ea3f13aa4b0beee5de91a062c6800307
Navod Amarakoon <nexza@Users-MacBook-3.local>
2024-07-14 23:49:38 +0530

    Template winning

--- src/config/template.js
+++ src/config/template.js
@@ -1,14 +1,14 @@
-// module.exports = {
-//     newPlayer: `Thank you for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play this game https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
-//     existingPlayer: `Thank you again for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play this game https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
-//     darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. {voucher}`,
-//     reloadWinning: `Congratulations. You have won Rs.{winningPrize} from CENTER FRESH RELOAD WAVE.Keep playing and keep winning.`,
-// };
-
 module.exports = {
-    newPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
-    existingPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
-    darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. Voucher Code {voucher}`,
+    newPlayer: `Thank you for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play this game https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
+    existingPlayer: `Thank you again for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play this game https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
+    darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. {voucher}`,
     reloadWinning: `Congratulations. You have won Rs.{winningPrize} from CENTER FRESH RELOAD WAVE.Keep playing and keep winning.`,
 };
+
+// module.exports = {
+//     newPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
+//     existingPlayer: `Thank you for your message. The competition starts on the 15th of July. Please stay in touch with us.`,
+//     darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. Voucher Code {voucher}`,
+//     reloadWinning: `Congratulations. You have won Rs.{winningPrize} from CENTER FRESH RELOAD WAVE.Keep playing and keep winning.`,
+// };
 
