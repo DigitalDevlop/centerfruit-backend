f6ddbfcbd80660e72969cfeca0d7fb4f18e9c1ef
Navod Pasindu Amarakoon <51057557+nexzanavod@users.noreply.github.com>
2025-08-20 21:49:06 +0530

    Update template.js

--- src/config/template.js
+++ src/config/template.js
@@ -8,7 +8,7 @@
 
 module.exports = {
     newPlayer: `Thank you for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play. https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
-    existingPlayer: `Thank you again for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play.https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
+    existingPlayer: `Thank you again for the message for CENTER FRESH RELOAD WAVE. Please use {otp} code to play. https://centerfresh.digitalxlabs.com/?mobile={mobile}&otp={otp}.`,
     darazWinning: `Congratulations. You have won Rs.1,000/= Gift voucher from CENTER FRESH RELOAD WAVE. Purchase from Daraz.lk and claim your Voucher. {voucher}`,
     reloadWinning: `Congratulations. You have won Rs.{winningPrize} from CENTER FRESH RELOAD WAVE.Keep playing and keep winning.`,
     attemptMSG: `Thank you for participating in the CENTER FRESH RELOAD WAVE. Your OTP attempts have exceeded today's limit. Please try again tomorrow.`,
